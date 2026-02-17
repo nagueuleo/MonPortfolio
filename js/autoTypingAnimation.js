98d199ea575f0116b0eb59b8df5b6d6b81be179e
Nagueu Lionel <nagueu.datascientist@gmail.com>
2026-02-17 11:24:27 +0100

    V2.0

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,8 +1,17 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Hello, Je suis NAGUEU Lionel</span> Etudiant en master I et Passionné par l’analyse de données et l’intelligence artificielle, je suis un Data Scientist doté d’une solide maîtrise des outils et techniques de traitement des données. ayant des connaissances en Python, R, SQL, Machine Learning, et visualisation de données, j’ai mené divers projets académiques et professionnels, tels que le développement de modèles prédictifs et la création de tableaux de bord interactifs. Mon approche combine rigueur analytique, créativité et esprit d’équipe pour transformer des données en solutions stratégiques et exploitables"
-      ],
+  `<span>Hello, Je suis Lionel NAGUEU</span> titulaire d'un Master RNCP en
+Data et Intelligence Artificielle j'ai acquis une expérience de deux
+ans et demi dans le domaine de l'analyse de données. Je suis
+compétent dans l'utilisation d'outils statistiques et de visualisation
+comme Python, SQL, dbt et Power BI. Mon approche rigoureuse et
+axée sur les résultats m'a permis de diriger des projets complexes
+d'analyse de données ainsi que de concevoir des rapports
+décisionnels sous forme de tableaux de bord. Aujourd'hui, je
+souhaite mettre à profit mon expertise technique et mes
+compétences analytiques au sein d'un equipe dynamique et innovatrice.`
+],
       typeSpeed: 7,
       contentType: 'html'
     });
